630b4c0e40cfc8cdf11bf96cf257c37169b4ebdf
William Ambriz <Wambriz@me.com>
2023-11-18 14:58:40 -0800

    Seach function pops up

--- LandmarkFinder/SearchView.swift
+++ LandmarkFinder/SearchView.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 import MapKit
 
+struct Landmark: Identifiable {
+    var id = UUID()
+    var name: String
+    var location: CLLocationCoordinate2D
+}
+
 struct SearchView: View {
     @State private var searchText = ""
     @StateObject private var locationManager = LocationManager()
@@ -8,17 +14,17 @@ struct SearchView: View {
 
     var body: some View {
         VStack {
-            // Search Bar
-            TextField("Search landmarks", text: $searchText)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-                .padding()
+            TextField("Search landmarks", text: $searchText, onCommit: {
+                performSearch(query: searchText)
+            })
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding()
 
             // Splitting screen for results and map
             GeometryReader { geometry in
                 VStack(spacing: 0) {
-                    // Search Results
                     List(searchResults, id: \.id) { landmark in
                         Text(landmark.name)
                             .onTapGesture {
@@ -27,21 +33,33 @@ struct SearchView: View {
                     }
                     .frame(height: geometry.size.height / 2)
 
-                    // Map View
                     Map(coordinateRegion: $locationManager.region, showsUserLocation: true)
-                        .frame(height: geometry.size.height / 1.81)
+                        .frame(height: geometry.size.height / 2)
+                        .accentColor(Color(.systemPink))
                 }
             }
         }
         .navigationBarTitle("Search Landmarks", displayMode: .inline)
     }
 
-}
+    func performSearch(query: String) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = query
+        // Use the current region from locationManager for search
+        request.region = locationManager.region
 
-struct Landmark {
-    var id: Int
-    var name: String
-    var location: CLLocationCoordinate2D
+        let search = MKLocalSearch(request: request)
+        search.start { response, error in
+            guard let response = response else {
+                print("Error: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+
+            searchResults = response.mapItems.map { item in
+                Landmark(name: item.name ?? "", location: item.placemark.coordinate)
+            }
+        }
+    }
 }
 
 struct SearchView_Previews: PreviewProvider {
@@ -49,6 +67,7 @@ struct SearchView_Previews: PreviewProvider {
         SearchView()
     }
 }
+
 
 
 
