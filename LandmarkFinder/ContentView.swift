0378a7e149ff76416eeca646f97a52e713af0ba6
William Ambriz <Wambriz@me.com>
2023-11-17 17:43:15 -0800

    Some what function sidebar for futur saved locations

--- LandmarkFinder/ContentView.swift
+++ LandmarkFinder/ContentView.swift
@@ -6,31 +6,80 @@ struct ContentView: View {
         center: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868),
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
+    @State private var isShowingSidebar = false
 
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "line.horizontal.3")
-                    .imageScale(.large)
-                    .padding(.leading)
+        NavigationView {
+            GeometryReader { geometry in
+                ZStack(alignment: .topLeading) {
+                    VStack(spacing: 0) {
+                        // Header
+                        HStack {
+                            Button(action: {
+                                withAnimation {
+                                    isShowingSidebar.toggle()
+                                }
+                            }) {
+                                Image(systemName: "line.horizontal.3")
+                                    .imageScale(.large)
+                                    .padding()
+                            }
 
-                Spacer()
+                            Spacer()
 
-                Text("Landmark Finder")
-                    .font(.title)
-                    .fontWeight(.bold)
+                            Text("Landmark Finder")
+                                .font(.title)
+                                .fontWeight(.bold)
 
-                Spacer()
+                            Spacer()
 
-                Image(systemName: "magnifyingglass")
-                    .imageScale(.large)
-                    .padding(.trailing)
+                            Image(systemName: "magnifyingglass")
+                                .imageScale(.large)
+                                .padding()
+                        }
+                        .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0)
+                        .background(Color(UIColor.systemBackground))
+                        .zIndex(1)
+
+                        // Map
+                        Map(coordinateRegion: $region)
+                            .edgesIgnoringSafeArea(.bottom)
+                    }
+
+                    // Transparent background for dismissing the sidebar
+                    if isShowingSidebar {
+                        Color.black.opacity(0.4)
+                            .edgesIgnoringSafeArea(.all)
+                            .onTapGesture {
+                                withAnimation {
+                                    isShowingSidebar = false
+                                }
+                            }
+                            .zIndex(0) // Ensures the overlay is below the sidebar
+                    }
+
+                    // Sidebar
+                    if isShowingSidebar {
+                        SidebarView()
+                            .frame(width: geometry.size.width / 2)
+                            .transition(.move(edge: .leading))
+                            .background(Color(UIColor.systemBackground))
+                            .zIndex(1) // Ensure the sidebar is above the overlay
+                    }
+                }
             }
-            .padding(.vertical)
-
-            Map(coordinateRegion: $region)
-                .edgesIgnoringSafeArea(.all)
+            .navigationBarHidden(true)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct SidebarView: View {
+    var body: some View {
+        List {
+            Text("Saved Locations")
+        }
+        .listStyle(SidebarListStyle())
     }
 }
 
@@ -39,4 +88,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
 
