ed4515dae0d95503d9027ae67e8122cfaaa6be07
William Ambriz <Wambriz@me.com>
2023-11-17 17:02:31 -0800

    Initial Commit

---
+++ LandmarkFinder/LandmarkFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkFinderApp.swift
+//  LandmarkFinder
+//
+//  Created by William Ambriz on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
